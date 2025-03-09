d115f137d0c1453cce729986aeb92029c1720ca5
adams <adamskiptalam0@gmail.com>
2025-03-09 15:59:43 +0300

    Build the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-// import { useState } from 'react'
-import './App.css'
+import Header from './Components/Header';
 
 function App() {
+    return (
+        <>
+        <Header />
+        </>
+    )
 }
 export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <header className="header-container">
+            <h1>CV Generator App</h1>
+            <p>Generate your CV quickly with ease</p>
+        </header>
+    )
+}
