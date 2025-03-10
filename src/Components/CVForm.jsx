812c4d42ff83bbd4827f9010f2c1832db29a9989
adams <adamskiptalam0@gmail.com>
2025-03-10 18:13:03 +0300

    Added parent component to handle submitting and editing.

---
+++ src/Components/CVForm.jsx
@@ -0,0 +1,52 @@
+import GeneralInfo from './General.jsx';
+import Education from './Education.jsx';
+import Experience from './PracticalExp.jsx';
+import { useState } from 'react';
+
+
+export default function CVForm() {
+    const [isEditing, setIsEditing] = useState(true);
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        school: "",
+        title: "",
+        date: "",
+        company: "",
+        position: "",
+        responsibilities: "",
+        from: "",
+        to: "",
+    });
+
+    function handleChange (event) {
+        const {name, value} = event.target;
+        setFormData((prevInfo) => ({...prevInfo, [name]: value}));
+    }
+    function handleSubmit (event) {
+        event.preventDefault(); 
+        setIsEditing(false);
+    }
+    function handleEdit () {
+        setIsEditing(true);
+    }
+    
+    return (
+        <div className='cv-container'>
+            <GeneralInfo data={formData} handleChange={handleChange} isEditing={isEditing} />
+            <Education data={formData} handleChange={handleChange} isEditing={isEditing} />
+            <Experience data={formData} handleChange={handleChange} isEditing={isEditing} />
+
+            <div className='button-groups'>
+                {isEditing? (
+                    <button type='submit' onClick={handleSubmit}>Submit</button>
+                ) : (
+                    <button type='button' onClick={handleEdit}>Edit</button>
+                )}
+            </div>
+        </div>
+    )
+}
+
